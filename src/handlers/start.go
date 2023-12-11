49d8a11c8b773c1b3085dc7ac5a9e42499ea4a4e
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2023-12-12 06:04:54 +0800

    add native slices.Contains method to start handler

--- src/handlers/start.go
+++ src/handlers/start.go
@@ -4,6 +4,7 @@ import (
 	messageUtils "fast_dolphin_telegram_bot/messages"
 	"fast_dolphin_telegram_bot/src/utils"
 	"fmt"
+	"slices"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -17,7 +18,7 @@ func Start(bot *tgbotapi.BotAPI, update tgbotapi.Update, config *utils.Config, m
 		greetingMessage = fmt.Sprintf(messages.StartMessageAdmin, config.AdminNameEvg)
 	case fmt.Sprintf("%d", userChatID) == config.AdminChatIDNat:
 		greetingMessage = fmt.Sprintf(messages.StartMessageAdmin, config.AdminNameNat)
-	case contains(config.CoachChatIDs, fmt.Sprintf("%d", userChatID)):
+	case slices.Contains(config.CoachChatIDs, fmt.Sprintf("%d", userChatID)):
 		greetingMessage = messages.StartMessageCoach
 	default:
 		greetingMessage = messages.StartMessage
@@ -25,14 +26,4 @@ func Start(bot *tgbotapi.BotAPI, update tgbotapi.Update, config *utils.Config, m
 
 	msg := tgbotapi.NewMessage(userChatID, greetingMessage)
 	bot.Send(msg)
-}
-
-// contains checks if a slice contains a specific string.
-func contains(slice []string, item string) bool {
-	for _, v := range slice {
-		if v == item {
-			return true
-		}
-	}
-	return false
 }
